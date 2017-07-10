3681cd1bf7b87bae706f6bc7d537fa5bb041c665
Sylvain Boilard <boilard@crans.org>
2017-07-10 22:28:06 +0200

    Perform bounds checking ourselves in symetric arrays.
    The way indexing is done in symetric arrays, relying on OCaml arrays bounds
    checking would almost never catch negative indexes.

--- symetricArray.ml
+++ symetricArray.ml
@@ -9,13 +9,19 @@ let make l e =
   try { length = l; data = Array.make (l * (l + 1) / 2) e }
   with Invalid_argument _ -> invalid_arg "SymetricArray.make"
 
-let get a ((i, j) as c) =
+let get a c =
   let i, j = norm_coords c in
-  a.data.(i + j * (j + 1) / 2)
+  if i < 0 || j >= a.length then
+    invalid_arg "index out of bounds"
+  else
+    Array.unsafe_get a.data (i + j * (j + 1) / 2)
 
-let set a ((i, j) as c) e =
+let set a c e =
   let i, j = norm_coords c in
-  a.data.(i + j * (j + 1) / 2) <- e
+  if i < 0 || j >= a.length then
+    invalid_arg "index out of bounds"
+  else
+    Array.unsafe_set a.data (i + j * (j + 1) / 2) e
 
 let unsafe_get a c =
   let i, j = norm_coords c in
