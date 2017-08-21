dc08af4e5d77b599103d6fccba3bea2eeacf5739
Sylvain Boilard <boilard@crans.org>
2017-08-21 18:36:51 +0200

    Changed inner types of Heap module (should speed up meld function a bit).

--- heap.ml
+++ heap.ml
@@ -18,33 +18,36 @@ module type S =
 module Make (Ord: OrderedType) =
   struct
     type elt = Ord.t
-    type t = EmptyHeap | Heap of elt * t list
+    type h = H of elt * h list
+    type t = h option
+
     exception Empty
 
-    let empty = EmptyHeap
+    let empty = None
 
-    let rec meld acc = function
-      | EmptyHeap::_ | _::EmptyHeap::_ -> assert false
-      | h::[] when acc = [] -> h
-      | [] -> assert (acc <> []); meld [] acc
-      | h::[] -> meld [] (h::acc)
-      | (Heap (e1, l1) as h1)::(Heap (e2, l2) as h2)::tl ->
+    let rec meld l' l =
+      match l', l with
+      | [], h :: [] | h :: [], [] -> h
+      | _, h :: [] -> meld [] (h :: l')
+      | _, [] -> assert (l' <> []); meld [] l'
+      | _, (H (e1, l1) as h1) :: (H (e2, l2) as h2) :: tl ->
          if Ord.compare e1 e2 < 0 then
-           meld (Heap (e1, h2::l1)::acc) tl
+           meld (H (e1, h2 :: l1) :: l') tl
          else
-           meld (Heap (e2, h1::l2)::acc) tl
+           meld (H (e2, h1 :: l2) :: l') tl
               
     let insert e = function
-      | EmptyHeap -> Heap (e, [])
-      | Heap (e', l) when Ord.compare e' e < 0 -> Heap (e', Heap (e, [])::l)
-      | h -> Heap (e, [h])
+      | None -> Some (H (e, []))
+      | Some (H (e', l)) when Ord.compare e' e < 0 ->
+         Some (H (e', H (e, []) :: l))
+      | Some h -> Some (H (e, [h]))
 
     let pop = function
-      | EmptyHeap -> raise Empty
-      | Heap (_, []) -> EmptyHeap
-      | Heap (_, l) -> meld [] l
+      | None -> raise Empty
+      | Some (H (_, [])) -> None
+      | Some (H (_, l)) -> Some (meld [] l)
 
     let top = function
-      | EmptyHeap -> raise Empty
-      | Heap (e, _) -> e
+      | None -> raise Empty
+      | Some (H (e, _)) -> e
   end
